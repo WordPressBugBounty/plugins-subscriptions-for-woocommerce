13e02dfd303ceb593bf16a0333e6925bf2713eae
Sean Murphy <sean@iamseanmurphy.com>
2024-11-22 13:20:30 +0000

    1.7.3

--- subscriptions-for-woocommerce/build/index.asset.php
+++ subscriptions-for-woocommerce/build/index.asset.php
@@ -1,17 +1,1 @@
-<?php
-/**
- * Provide a admin area view for the plugin
- *
- * This file is used to markup the html field for general tab.
- *
- * @link       https://wpswings.com/
- * @since      1.0.0
- *
- * @package    Subscriptions_For_Woocommerce
- * @subpackage Subscriptions_For_Woocommerce/build
- */
-
-return array(
-	'dependencies' => array( 'wp-element', 'wp-polyfill' ),
-	'version' => 'c18eb6767e641a7522507a86d9c71475',
-);
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'f6390c7385fa7a0260cc40cedf4c63f1');
